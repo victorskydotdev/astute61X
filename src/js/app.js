37c9e35c0ea3da5179a6c629d81e1434942a0e3c
techyVS <victorsky90@gmail.com>
2023-08-09 17:06:51 +0100

    made updates and added the navbar, though buggy, will fix it

---
+++ src/js/app.js
@@ -0,0 +1,24 @@
+const navbar = document.querySelector('.navigation');
+const navOpenBtn = document.querySelector('.hamburger');
+const navCloseBtn = document.querySelector('.hamburger-close');
+
+const openNavbar = () => {
+	navOpenBtn.addEventListener('click', () => {
+		navbar.style.display = 'block';
+
+		navOpenBtn.style.display = 'none';
+		navCloseBtn.style.display = 'flex';
+	});
+};
+
+const closeNavbar = () => {
+	navCloseBtn.addEventListener('click', () => {
+		navbar.style.display = 'none';
+
+		navOpenBtn.style.display = 'flex';
+		navCloseBtn.style.display = 'none';
+	});
+};
+
+openNavbar();
+closeNavbar();
